01930d0b26ddc01ac61343eeea3040ee3b154392
Sander van den Hoogen <81399@rocteraa-student.nl>
2020-03-09 13:33:57 +0100

    Initial commit

---
+++ GradeCalculator/GradeCalculator.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace GradeCalculator
+{
+    partial class frmGradeCalculatorShoo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbGradingMethodShoo = new System.Windows.Forms.GroupBox();
+            this.grbGradeOutputShoo = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grbGradingMethodShoo.SuspendLayout();
+            this.grbGradeOutputShoo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grbGradingMethodShoo
+            // 
+            this.grbGradingMethodShoo.Controls.Add(this.radioButton2);
+            this.grbGradingMethodShoo.Controls.Add(this.radioButton1);
+            this.grbGradingMethodShoo.Controls.Add(this.label3);
+            this.grbGradingMethodShoo.Controls.Add(this.textBox2);
+            this.grbGradingMethodShoo.Controls.Add(this.label2);
+            this.grbGradingMethodShoo.Controls.Add(this.textBox1);
+            this.grbGradingMethodShoo.Controls.Add(this.label1);
+            this.grbGradingMethodShoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbGradingMethodShoo.Location = new System.Drawing.Point(12, 12);
+            this.grbGradingMethodShoo.Name = "grbGradingMethodShoo";
+            this.grbGradingMethodShoo.Size = new System.Drawing.Size(521, 78);
+            this.grbGradingMethodShoo.TabIndex = 0;
+            this.grbGradingMethodShoo.TabStop = false;
+            this.grbGradingMethodShoo.Text = "Grading method";
+            // 
+            // grbGradeOutputShoo
+            // 
+            this.grbGradeOutputShoo.Controls.Add(this.button1);
+            this.grbGradeOutputShoo.Controls.Add(this.label10);
+            this.grbGradeOutputShoo.Controls.Add(this.label9);
+            this.grbGradeOutputShoo.Controls.Add(this.label8);
+            this.grbGradeOutputShoo.Controls.Add(this.label7);
+            this.grbGradeOutputShoo.Controls.Add(this.label6);
+            this.grbGradeOutputShoo.Controls.Add(this.label5);
+            this.grbGradeOutputShoo.Controls.Add(this.label4);
+            this.grbGradeOutputShoo.Controls.Add(this.textBox3);
+            this.grbGradeOutputShoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbGradeOutputShoo.Location = new System.Drawing.Point(12, 96);
+            this.grbGradeOutputShoo.Name = "grbGradeOutputShoo";
+            this.grbGradeOutputShoo.Size = new System.Drawing.Size(521, 99);
+            this.grbGradeOutputShoo.TabIndex = 1;
+            this.grbGradeOutputShoo.TabStop = false;
+            this.grbGradeOutputShoo.Text = "Grade output";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maximum points";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 48);
+            this.textBox1.MaxLength = 9;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 24);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(174, 48);
+            this.textBox2.MaxLength = 9;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 24);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "5.5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ceasura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(338, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Grade calculation method";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(341, 49);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 22);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Linear";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(430, 49);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 22);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Parabola";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Calculate grade";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 66);
+            this.textBox3.MaxLength = 9;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 24);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "at # points";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(171, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(125, 18);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Grade at # points:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(171, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 18);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Points:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(171, 69);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 18);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Grade:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(231, 45);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 18);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "...";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(230, 69);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(20, 18);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 64);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Generate PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // frmGradeCalculatorShoo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grbGradeOutputShoo);
+            this.Controls.Add(this.grbGradingMethodShoo);
+            this.Name = "frmGradeCalculatorShoo";
+            this.Text = "Grade Calculator";
+            this.grbGradingMethodShoo.ResumeLayout(false);
+            this.grbGradingMethodShoo.PerformLayout();
+            this.grbGradeOutputShoo.ResumeLayout(false);
+            this.grbGradeOutputShoo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbGradingMethodShoo;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grbGradeOutputShoo;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
